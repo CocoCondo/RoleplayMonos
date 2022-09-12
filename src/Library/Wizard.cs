e35095e316c2afd2b485e31b1d29f26e9f73e3cc
Santiagoal1208 <santiagoalmagro1208@gmail.com>
2022-09-12 19:52:58 -0300

    Clase Wizard

--- src/Library/Wizard.cs
+++ src/Library/Wizard.cs
@@ -18,7 +18,7 @@ using System.Collections.Generic;
         }
         public static void Add_Item(Wizard wiz, Item item){
             if(wiz.Items.Contains(item)){
-            wiz.Items.remove(item);
+            wiz.Items.Remove(item);
             }
         
         }
