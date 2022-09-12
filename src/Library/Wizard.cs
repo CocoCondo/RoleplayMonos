b43750689dc014f664b636cf423e6feb3e4863cc
Santiagoal1208 <santiagoalmagro1208@gmail.com>
2022-09-12 20:27:18 -0300

    Clase Wizard

--- src/Library/Wizard.cs
+++ src/Library/Wizard.cs
@@ -8,19 +8,23 @@ namespace MonosAnillos;
         int Hp {get; set;}
         List <Item> Items {get; set;}
 
+        int Ataque {get; set;}
+        
+
         public Wizard( string nombre,int salud){
             this.Name=nombre;
             this.Hp=salud;
+            this.Ataque=0;
 
         }
         
         public void Add_Item(Item item){
             this.Items.Add(item);
         }
-        public static void Add_Item(Wizard wiz, Item item){
+        public void Remove_Item(Wizard wiz, Item item){
             if(wiz.Items.Contains(item)){
             wiz.Items.Remove(item);
             }
-        
         }
+        
     }
