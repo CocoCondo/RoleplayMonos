8ee328262f58f611dc837e70e77bff795458bc91
CocoCondo <leonardo.conde@correo.ucu.edu.uy>
2022-09-12 20:09:09 -0300

    Agregado BoS

--- src/Library/Items/BookOfSpells.cs
+++ src/Library/Items/BookOfSpells.cs
@@ -2,17 +2,22 @@ namespace MonosAnillos;
 using System;
 using System.Collections.Generic;
 
-class BookOfSpell
+class BookOfSpells
 {
     private string Name {get; set;}
     private int Dmg {get; set;}
     private int Def {get; set;}
     private List<Spell> Spells {get; set;}
 
-    public BookOfSpell(string name, int dmg, int def)
+    public BookOfSpells(string name, int dmg, int def)
     {
         this.Name = name;
         this.Dmg = dmg;
         this.Def = def;
+    }
+
+    public void AddSpell(Spell spell)
+    {
+        this.Spells.Add(spell);
     }
 }
